8be9f44b4489fcce43ccfa6b8c3c9fd98a87c654
kelthame-1 <kelthame77@icloud.com>
2026-06-20 03:42:39 +0200

    fix auth bypass

--- src/main/java/com/travvite/backend/security/JwtFilter.java
+++ src/main/java/com/travvite/backend/security/JwtFilter.java
@@ -26,7 +26,15 @@ public class JwtFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain)
             throws ServletException, IOException {
+        String path = request.getServletPath();
 
+        if (path.startsWith("/api/auth/") ||
+                path.startsWith("/swagger-ui") ||
+                path.startsWith("/v3/api-docs")) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
